1fd3ca3bb6410de60967f8294b662f48d0f90de2
ナイトウコウスケ <102337893+naitokosuke@users.noreply.github.com>
2026-01-03 18:20:04 +0900

    issue/5 content schema valibot (#14)
    * chore(deps): valibot
    
    * chore(deps): add @valibot/to-json-schema dependency
    
    * refactor: update content schema to use valibot and remove unused collection

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 const route = useRoute();
 
-const { data: page } = await useAsyncData("page-" + route.path, () => {
-  return queryCollection("content").path(route.path).first();
+const { data: page } = await useAsyncData(() => {
+  return queryCollection("posts").path(route.path).first();
 });
 
-if (!page.value) {
+if (page.value != null) {
   throw createError({ statusCode: 404, statusMessage: "Page not found", fatal: true });
 }
 </script>
--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,20 @@
 import { defineContentConfig, defineCollection } from "@nuxt/content";
+import * as v from "valibot";
+
+const posts = defineCollection({
+  type: "page",
+  source: "**",
+  schema: v.object({
+    title: v.string(),
+    description: v.optional(v.string()),
+    date: v.optional(v.pipe(v.string(), v.isoDate())),
+    tags: v.optional(v.array(v.string()), []),
+    draft: v.optional(v.boolean(), false),
+  }),
+});
 
 export default defineContentConfig({
   collections: {
-    content: defineCollection({
-      type: "page",
-      source: "**",
-    }),
+    posts,
   },
 });
