570835f99ddbab8161c5b583716a4596545ecb7f
naitokosuke <kosuke.naito.engineer@gmail.com>
2026-01-03 21:46:49 +0900

    feat: add custom domain blog.naito.dev

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  nitro: {
-    preset: "cloudflare_module",
-    cloudflare: {
-      wrangler: {
-        compatibility_date: "2024-09-19",
-        d1_databases: [
-          {
-            binding: "DB",
-            database_name: "blog-content",
-            database_id: "ebfd80fa-49a3-43ca-9a27-0d37a66ef98b",
-          },
-        ],
-      },
-    },
-  },
   modules: [
     "@nuxt/content",
     "@nuxt/eslint",
@@ -35,7 +20,10 @@ export default defineNuxtConfig({
       nativeSqlite: true,
     },
   },
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-09-19",
+  nitro: {
+    preset: "cloudflare_module",
+  },
   eslint: {
     config: {
       stylistic: {
